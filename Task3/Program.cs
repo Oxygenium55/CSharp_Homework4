3c5318481d6ab14d457fe200c0c910e12528b82e
Oxygenium55 <114757285+Oxygenium55@users.noreply.github.com>
2022-10-27 21:44:07 +0600

    Task3 done

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+пример из 5 элементов:
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+получить все 8 чисел за одно нажатие Enter от пользователя.подсказка: использовать метод Split();
+*/
+
+Console.WriteLine("Введите 8 чисел: ");
+string s = Console.ReadLine();
+int count = 8;
+string[] Array = s.Split(", ", count, StringSplitOptions.RemoveEmptyEntries);
+
+Console.Write("[");
+for(int i = 0; i < Array.Length; i++)
+{
+    if(i < Array.Length -1) Console.Write($"{Array[i]}, ");
+    else Console.WriteLine($"{Array[i]}]");
+}
